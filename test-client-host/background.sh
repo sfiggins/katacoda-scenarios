ad1f4dad126882e697186bbaa17306bc8b1df472
Stephen Figgins <sfiggins@oreilly.com>
2021-03-16 15:28:23 -0500

    tweak script to test for exec.

--- test-client-host/background.sh
+++ test-client-host/background.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh root@host01 "until [ -f /usr/local/bin/run_server.sh ]; do sleep 1; done; /usr/local/bin/run_server.sh"
+ssh root@host01 'until [ -x /usr/local/bin/run_server.sh ]; do sleep 1; done; /usr/local/bin/run_server.sh'
