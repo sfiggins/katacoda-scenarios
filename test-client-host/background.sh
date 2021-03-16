6fb7488830b2c130cad765e2f4ab6a78cbfb9bf2
Stephen Figgins <sfiggins@oreilly.com>
2021-03-16 16:45:06 -0500

    experiment with client/host01; use nohup for ssh

--- test-client-host/background.sh
+++ test-client-host/background.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-ssh root@host01 'until [ -x /usr/local/bin/run_server.sh ]; do sleep 1; done; /usr/local/bin/run_server.sh'
+ssh root@host01 <<EOF
+nohup bash -c "until [ -x /usr/local/bin/run_server.sh ]; do sleep 1; done; /usr/local/bin/run_server.sh" > /tmp/run_server.out 2>&1 &
+EOF
