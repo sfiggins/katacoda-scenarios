a49bcd7da952bb2212acf127e30fba0a375b3d7f
Stephen Figgins <sfiggins@oreilly.com>
2021-03-17 10:13:30 -0500

    try background docker compose

--- test-client-host/background.sh
+++ test-client-host/background.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # wait for asset to show up
-until [ -x /root/run_server.sh ]
+until [ -x /root/docker-compose.yml ]
  do sleep 1
 done
 
-ssh root@host01 bash -s < /root/run_server.sh
+ssh root@host01 /usr/local/bin/docker-compose -d < /root/docker-compose.yml
