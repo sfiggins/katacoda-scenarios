2e6ab5fef228da7ec99930f2cdd18a1f64533f5c
Stephen Figgins <sfiggins@oreilly.com>
2021-03-17 10:46:55 -0500

    lower docker-compose version; put dc args in order

--- test-client-host/background.sh
+++ test-client-host/background.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
-
 # wait for asset to show up
-until [ -x /root/docker-compose.yml ]
+until [ -f /root/docker-compose.yml ]
  do sleep 1
 done
 
-ssh root@host01 /usr/local/bin/docker-compose -d < /root/docker-compose.yml > /tmp/ssh-dc.log
+ssh root@host01 /usr/local/bin/docker-compose -f - up -d < /root/docker-compose.yml > /tmp/ssh-dc.log
