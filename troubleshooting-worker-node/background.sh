2ebc5a2ed537560e65c870e4422b74235e1a0077
Stephen Figgins <sfiggins@oreilly.com>
2022-03-25 10:04:40 -0500

    Revert "swap foreground and background script."
    This reverts commit 5ade5251f94155b7b956bd38ecf973eb6ed19ff6.

--- troubleshooting-worker-node/background.sh
+++ troubleshooting-worker-node/background.sh
@@ -1,1 +1,24 @@
-echo "no background script" >> /opt/katacoda-background.log
+#!/bin/bash
+set -v
+
+while true; do 
+  grep "DoneDone" /opt/katacoda-finished &> /dev/null
+  if [[ "$?" -ne 0 ]]; then
+    sleep 1
+  else
+    break
+  fi
+done
+
+ssh node01 "
+echo 'wait for file'
+until [ -f /var/lib/kubelet/config.yaml ]
+do
+  sleep 1
+done
+echo 'file exists; running sed'
+sed -i 's/clientCAFile: \/etc\/kubernetes\/pki\/ca.crt/clientCAFile: \/etc\/kubernetes\/pki\/non-existent-ca.crt/g' /var/lib/kubelet/config.yaml
+echo 'reloading daemon and restarting kubelet'
+systemctl daemon-reload
+systemctl restart kubelet
+" >> /opt/katacoda-background.log 2>&1
--- troubleshooting-worker-node/foreground.sh
+++ troubleshooting-worker-node/foreground.sh
@@ -1,14 +1,1 @@
 launch.sh
-
-ssh node01 "
-echo 'wait for file'
-until [ -f /var/lib/kubelet/config.yaml ]
-do
-  sleep 1
-done
-echo 'file exists; running sed'
-sed -i 's/clientCAFile: \/etc\/kubernetes\/pki\/ca.crt/clientCAFile: \/etc\/kubernetes\/pki\/non-existent-ca.crt/g' /var/lib/kubelet/config.yaml
-echo 'reloading daemon and restarting kubelet'
-systemctl daemon-reload
-systemctl restart kubelet
-" >> /opt/katacoda-foreground.log 2>&1
